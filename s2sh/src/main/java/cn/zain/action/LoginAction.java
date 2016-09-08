80452cb9dde8cb4b6fa29a0815add5ca33b823a6
Zain <yongzhian@163.com>
2016-09-08 14:01:46 +0800

    修复po toString的null 添加版本说明

--- s2sh/src/main/java/cn/zain/action/LoginAction.java
+++ s2sh/src/main/java/cn/zain/action/LoginAction.java
@@ -5,39 +5,44 @@ package cn.zain.action;
  */
 
 import cn.zain.action.base.BaseAction;
+import cn.zain.model.po.SysUser;
 import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ModelDriven;
 import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+
+import javax.servlet.ServletContext;
+
 /**
  * Created by Zain 2016/9/7 .
  */
-public class LoginAction extends BaseAction {
+public class LoginAction extends BaseAction implements ModelDriven {
     private Logger logger = Logger.getLogger(LoginAction.class);
 
-    private String username;
-    private String password;
+    private SysUser sysUser;
 
     @Override
     public String execute() throws Exception {
                 logger.info("hhh34567890");
-        if ("haha".equals(username) && "hehe".equals(password))// 如果登录的用户名=haha并且密码=hehe，就返回SUCCESS；否则，返回LOGIN
+        if ("haha".equals(sysUser.getUsername()) && "hehe".equals(sysUser.getPassword()))// 如果登录的用户名=haha并且密码=hehe，就返回SUCCESS；否则，返回LOGIN
             return SUCCESS;
         return LOGIN;
     }
 
 
-    public String getUsername() {
-        return username;
+    @Override
+    public Object getModel() {
+//        String s1 = ActionContext.getContext().get("username");
+//        String s2 =  ServletActionContext.getContext().get("username");
+         String s3 = request.getParameter("username");
+        if(null == sysUser){
+            sysUser = new SysUser();
+            return sysUser;
+        }
+        return null;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public SysUser getSysUser() {
+        return sysUser;
     }
 }
---
+++ s2sh/src/test/java/cn/zain/base/BasicJdk.java
@@ -0,0 +1,27 @@
+package cn.zain.base;
+
+import cn.zain.model.po.SysUser;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * Copyright (c) 2016 www.yongzhian.cn. All Rights Reserved.
+ */
+public class BasicJdk {
+    private static Logger logger = Logger.getLogger(BasicJdk.class);
+    /**
+     * 功能说明 ：todo
+     * @author	Zain 2016/9/8 13:54
+     * @return
+     * @params
+     */
+    @Test
+    public void stringTest() throws Exception {
+        Object o = null;
+        SysUser sysUser = new SysUser();
+        sysUser.setFullName("tt");;
+        logger.info("value Of sysUser:" + String.valueOf(sysUser));
+//        logger.info(" null:" + String.valueOf(null));
+//        logger.info("value Of null:" + String.valueOf(o));
+    }
+}
