e61d320f9f0629accc8f0edeedd65d7d387bb0fa
Zain <yongzhian@163.com>
2016-09-26 11:55:17 +0800

    spring boot简单入门

---
+++ springboot/src/main/java/cn/zain/controller/SampleController.java
@@ -0,0 +1,24 @@
+package cn.zain.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Copyright (c) 2016 www.yongzhian.cn. All Rights Reserved.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController  {
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleController.class,args);
+    }
+}
