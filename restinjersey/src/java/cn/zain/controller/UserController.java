bf0715444a071b41dee7cf351f53661c96486531
Zain <yongzhian@163.com>
2016-09-29 10:11:33 +0800

    jersey demo

---
+++ restinjersey/src/java/cn/zain/controller/UserController.java
@@ -0,0 +1,27 @@
+package cn.zain.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ * Copyright (c) 2016 www.yongzhian.cn. All Rights Reserved.
+ */
+@Path("/user")
+public class UserController {
+
+    @GET
+    @Path("/info")
+    @Produces("text/plain;charset=UTF-8") //解决乱码问题
+    public String userInfo() {
+        return "张三 32岁";
+    }
+
+    @GET
+    @Path("/{param}")
+    @Produces("text/plain;charset=UTF-8") //解决乱码问题
+    public String sayHelloToUTF8(@PathParam("param") String username) {
+        return "你好 " + username;
+    }
+}
