8133995cbc67c12978d099028c1657e9f1295ecc
Zain <yongzhian@163.com>
2016-10-17 11:10:15 +0800

    添加UTC时间转换

--- myfx/src/main/java/cn/zain/controller/MainPanelController.java
+++ myfx/src/main/java/cn/zain/controller/MainPanelController.java
@@ -11,6 +11,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 /**
@@ -37,11 +38,11 @@ public class MainPanelController implements Initializable {
     }
 
     @FXML
-    private void buttonCalAction(ActionEvent event){
+    private void buttonCalAction(ActionEvent event) {
         Choice choice = Choice.valueOf(choiceBox.getValue().toString());
         String str = inputText.getText();
-        if(StringUtils.isNotBlank(str)){
-            switch (choice){
+        if (StringUtils.isNotBlank(str)) {
+            switch (choice) {
                 case 驼峰形式:
                     outputText.setText(StringTools.toCamelCase(str));
                     status.setText("success");
@@ -54,29 +55,44 @@ public class MainPanelController implements Initializable {
                     outputText.setText(str.toLowerCase());
                     status.setText("success");
                     break;
-                default :
+                case UTC转时间:
+                    try {
+                        String date = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date(Long.parseLong(str) * 1000));
+                        outputText.setText(date);
+                        status.setText("success");
+                    } catch (NumberFormatException e) {
+                        status.setText("failed\n" + e);
+                    }
+                    break;
+                default:
                     status.setText("failed 未知类型");
                     break;
             }
-        }else{
+        } else {
             status.setText("failed");
         }
     }
 
     private void init() {
         logger.info(choiceBox);
-        choiceBox.setItems(FXCollections.observableArrayList(Choice.驼峰形式,Choice.转大写,Choice.转小写) );
+        choiceBox.setItems(FXCollections.observableArrayList(Choice.驼峰形式, Choice.转大写, Choice.转小写,Choice.UTC转时间));
         choiceBox.setValue(Choice.驼峰形式);
 
     }
 }
-enum Choice{
+
+enum Choice {
     驼峰形式,
     转大写,
     转小写,
+    UTC转时间,
+    转十进制Unicode,
+    转Uncode_16,
+    转ASCII,
+    ASCII转char,
     NOVALUE;
 
-    public Choice parseStrToChoice(String str){
+    public Choice parseStrToChoice(String str) {
         try {
             return valueOf(str);
         } catch (Exception e) {
