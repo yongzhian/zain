3b4d74896e82774569a0c064ffdd8fc01c0cbe22
Zain <yongzhian@163.com>
2016-12-30 16:42:45 +0800

    创建简单的ssm项目

---
+++ ssm4/src/main/java/com/zain/controller/SysUserController.java
@@ -0,0 +1,33 @@
+package com.zain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Copyright (c) 2016 www.yongzhian.cn. All Rights Reserved.
+ */
+@Controller
+@RequestMapping("/sysuser")
+public class SysUserController {
+    @RequestMapping(value = "/get.do", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> get() {
+        Map<String, Object> returnMap = new HashMap<>();
+        returnMap.put("method", "get");
+        return returnMap;
+
+    }
+    @RequestMapping(value = "/post.do", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> count() {
+        Map<String, Object> returnMap = new HashMap<>();
+        returnMap.put("method", "post");
+        return returnMap;
+
+    }
+}
