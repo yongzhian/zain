e04bf3de569ba96836f19ee192cd8bf108670939
yong <yongzhian@163.com>
2017-11-20 13:48:07 +0800

    移除s2sh项目作为单独核心项目维护

--- s2sh/src/test/java/cn/zain/ditest/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Zain on 2016/10/10.
- * spring 依赖注入demo
- */
-package cn.zain.ditest;
--- s2sh/src/test/java/cn/zain/javaconfig/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Zain on 2016/10/10.
- * java配置方式 spring测试demo
- */
-package cn.zain.javaconfig;
