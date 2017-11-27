649673566e2767a124b866dbcd2e7dca635391b1
yong <yongzhian@163.com>
2017-11-27 19:45:09 +0800

    移除nlp项目作为单独核心项目维护 参考https://github.com/yongzhian/nlp

--- nlp/src/main/java/cn/zain/dao/RobotSceneWordDao.java
+++
@@ -1,7 +0,0 @@
-package cn.zain.dao;
-
-import cn.zain.model.entity.RobotSceneWord;
-
-public interface RobotSceneWordDao {
-    RobotSceneWord selectByPrimaryKey(Long id);
-}
--- nlp/src/main/java/cn/zain/dao/SysUserDao.java
+++
@@ -1,9 +0,0 @@
-package cn.zain.dao;
-
-import cn.zain.model.entity.SysUser;
-
-public interface SysUserDao {
-    SysUser selectByPrimaryKey(Long id);
-
-    SysUser selectByUsername(String username);
-}
