f77876acb4dbd3ba60722b8babe29392015ee02b
yong <yongzhian@163.com>
2017-12-20 11:46:13 +0800

    添加基于Jersey2.x的测试

---
+++ restinjersey/src/main/java/cn/zain/App.java
@@ -0,0 +1,44 @@
+package cn.zain;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * @author Zain
+ */
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) throws IOException {
+        ResourceConfig config = new ResourceConfig().packages("cn.zain.controller");
+        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create("http://localhost:8080/app/"), config);
+        logger.info("Start Grizzly Server success...");
+        System.in.read();
+        httpServer.shutdown();
+
+
+        //sun的jersey1.x版本 不推荐
+//        URI uri = UriBuilder.fromUri("http://127.0.0.1").port(8080).build();
+//        ResourceConfig rc = new PackagesResourceConfig("cn.zain.controller");
+//        HttpServer server = null;
+//        try {
+//            server = GrizzlyServerFactory.createHttpServer(uri, rc);
+//            server.start();
+//        } catch (IllegalArgumentException e) {
+//            e.printStackTrace();
+//        } catch (NullPointerException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        logger.info("Start Grizzly Server success...");
+//        System.in.read();
+//        server.stop();
+    }
+}
--- restinjersey/src/main/java/cn/zain/controller/UserController.java
+++ restinjersey/src/main/java/cn/zain/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Copyright (c) 2016 www.yongzhian.cn. All Rights Reserved.
@@ -13,7 +14,7 @@ public class UserController {
 
     @GET
     @Path("/info")
-    @Produces("text/plain;charset=UTF-8") //解决乱码问题
+    @Produces({MediaType.TEXT_PLAIN}) //解决乱码问题
     public String userInfo() {
         return "张三 32岁";
     }
